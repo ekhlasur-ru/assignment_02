8467c52507c3104d15571e50dc794db9fbac9e30
Ekhlasur Rahman <ekhlasur.ru@outlook.com>
2025-12-05 20:15:52 +0600

    Database design okay

--- src/config/db.ts
+++ src/config/db.ts
@@ -8,31 +8,38 @@ export const pool = new Pool({
 
 const initDB = async () => {
   await pool.query(`
-        CREATE TABLE IF NOT EXISTS users(
+        CREATE TABLE users(
         id SERIAL PRIMARY KEY,
         name VARCHAR(100) NOT NULL,
-        role VARCHAR(50) NOT NULL,
         email VARCHAR(150) UNIQUE NOT NULL,
-        password TEXT NOT NULL,
-        age INT,
-        phone VARCHAR(15),
-        address TEXT,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
+        password VARCHAR(200) NOT NULL CHECK (LENGTH(password) >= 6),
+        phone VARCHAR(20) NOT NULL,
+        role VARCHAR(20) NOT NULL CHECK (role IN ('admin', 'customer'))
         )
         `);
 
   await pool.query(`
-            CREATE TABLE IF NOT EXISTS todos(
+            CREATE TABLE vehicles(
             id SERIAL PRIMARY KEY,
-            user_id INT REFERENCES users(id) ON DELETE CASCADE,
-            title VARCHAR(200) NOT NULL,
-            description TEXT,
-            completed BOOLEAN DEFAULT false,
-            due_date DATE,
-            created_at TIMESTAMP DEFAULT NOW(),
-            updated_at TIMESTAMP DEFAULT NOW()
+            vehicle_name VARCHAR(100) NOT NULL,
+            type VARCHAR(20) NOT NULL CHECK(type IN ('car', 'bike', 'van', 'SUV')),
+            registration_number VARCHAR(50) UNIQUE NOT NULL,
+            daily_rent_price NUMERIC(10,2) NOT NULL CHECK (daily_rent_price > 0),
+            availability_status VARCHAR(20) NOT NULL CHECK (availability_status IN ('available', 'booked'))
             )
+            `);
+  await pool.query(`
+            CREATE TABLE bookings (
+            id SERIAL PRIMARY KEY,
+            customer_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+            vehicle_id INT NOT NULL REFERENCES vehicles(id) ON DELETE CASCADE,
+            rent_start_date DATE NOT NULL,
+            rent_end_date DATE NOT NULL,
+            total_price NUMERIC(10,2) NOT NULL CHECK (total_price > 0),
+            status VARCHAR(20) NOT NULL CHECK (status IN ('active', 'cancelled', 'returned')),
+            CHECK (rent_end_date > rent_start_date)
+        );
+
             `);
 };
 
