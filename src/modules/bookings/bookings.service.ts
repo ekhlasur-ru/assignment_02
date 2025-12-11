24281a10474dde82ad9cfb84f1d307d5898a012e
Ekhlasur Rahman <ekhlasur.ru@outlook.com>
2025-12-11 22:01:59 +0600

    final setup of deploy

--- src/modules/bookings/bookings.service.ts
+++ src/modules/bookings/bookings.service.ts
@@ -79,14 +79,7 @@ const updatebookingsID = async (id: string, payload: any) => {
   } = payload;
 
   const result = await pool.query(
-    `UPDATE bookings 
-     SET
-         rent_start_date=$1, 
-         rent_end_date=$2, 
-         total_price=$3, 
-         status=$4
-     WHERE id=$5 
-     RETURNING *`,
+    `UPDATE bookings SET rent_start_date=$1, rent_end_date=$2, total_price=$3, status=$4 WHERE id=$5 RETURNING *`,
     [rent_start_date, rent_end_date, total_price, status, id]
   );
 
